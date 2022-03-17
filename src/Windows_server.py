595024d7f927e0dd8ae633653d990c640733863f
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-03-17 16:46:37 +0100

    Update Windows_server.py

--- src/Windows_server.py
+++ src/Windows_server.py
@@ -7,5 +7,5 @@ def set_icon(tk):  # todo : Aquesta funció s'ha de cambiar
     with open(f"tmp_icon.ico", "wb+") as tmp2:
         tmp2.write(base64.b64decode(img))
         tk.iconbitmap(f"tmp_icon.ico")
-        os.remove(f"tmp_icon.ico")
+    os.remove(f"tmp_icon.ico")
 
